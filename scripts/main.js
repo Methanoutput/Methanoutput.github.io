071427b205b3d9e36699d4f903c287c8a61211c5
Jonas Hille <jonas@Mac-mini-von-Jonas.local>
2024-06-14 19:47:05 +0200

    random colors on mouse click

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,20 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Bye Bye : World!";
 
+// Funktion zur Generierung einer zufälligen Farbe
+function generateRandomColor() {
+    const randomColor = Math.floor(Math.random() * 0xFFFFFF);
+    const hexString = "#" + randomColor.toString(16).padStart(6, '0');
+    return hexString;
+}
+
+document.querySelector("html").addEventListener("click", function () {
+    // Zufällige Farbe generieren und setzen
+    const hexColor = generateRandomColor();
+    document.querySelector("body").style.backgroundColor = hexColor;
+});
+  
+
 /* 
     i
     am
@@ -12,4 +26,4 @@ myHeading.textContent = "Bye Bye : World!";
 
 let myVariable = "i am using arch btw.";
 
-alert(myVariable);
+// alert(myVariable); // -> makes super sketchy pop up.
