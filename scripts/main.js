f1815a0ec19ebb5085f5513cd3970f57e7421351
Jonas Hille <jonas@Mac-mini-von-Jonas.local>
2024-06-14 19:35:32 +0200

    add cool alert

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Bye Bye : World!";
+
+/* 
+    i
+    am
+    a
+    comment
+*/
+
+// this is also a comment
+
+let myVariable = "i am using arch btw.";
+
+alert(myVariable);
